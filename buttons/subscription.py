8c716b2da097840d1ae0f7e674fd33c37e30b4f6
Mahdi <mahdiiporheydarii@gmail.com>
2024-09-03 14:02:20 +0330

    Fix context

--- buttons/subscription.py
+++ buttons/subscription.py
@@ -71,22 +71,17 @@ async def show_subscription_options(update: Update, context: CallbackContext, qu
         [InlineKeyboardButton(text="Voice Model", callback_data="subscription_voice")],
         [InlineKeyboardButton(text="Image Model", callback_data="subscription_image")],
     ]
-    mysql = Mysql()
-    user_id = update.effective_user.id
-    user = mysql.getOne(f"select * from users where user_id={user_id}")
+
     reply_markup = InlineKeyboardMarkup(subscription_keyboard)
     if query and query.data == "subscription_options":
-        await query.edit_message_text(text=desired_plan[user['lang']], reply_markup=reply_markup)
+        await query.edit_message_text(text=desired_plan[context.user_data['lang']], reply_markup=reply_markup)
     else:
-        await update.message.reply_text(desired_plan[user['lang']], reply_markup=reply_markup)
+        await update.message.reply_text(desired_plan[context.user_data['lang']], reply_markup=reply_markup)
 
 async def show_subscription_plans(update: Update, context: CallbackContext):
     """Show available plans for the selected subscription model."""
     query = update.callback_query
     await query.answer()
-    mysql = Mysql()
-    user_id = update.effective_user.id
-    user = mysql.getOne(f"select * from users where user_id={user_id}")
     if query.data == "subscription_text":
         plans_keyboard = [
             [InlineKeyboardButton(text="GPT 4o", callback_data="gpt_4")],
@@ -111,14 +106,11 @@ async def show_subscription_plans(update: Update, context: CallbackContext):
         await show_subscription_options(update, context, query)
         return
     reply_markup = InlineKeyboardMarkup(plans_keyboard)
-    await query.edit_message_text(text=subscription_plan[user['lang']], reply_markup=reply_markup)
+    await query.edit_message_text(text=subscription_plan[context.user_data['lang']], reply_markup=reply_markup)
 
 async def gpt(update: Update, context: CallbackContext):
     query = update.callback_query
     await query.answer()
-    mysql = Mysql()
-    user_id = update.effective_user.id
-    user = mysql.getOne(f"select * from users where user_id={user_id}")
     if query.data == "gpt_4":
         plans_keyboard = [
             [InlineKeyboardButton(text="Economic Subscription - 49,900 IR Toman - 15 TRX", callback_data="plan_1")],
@@ -126,7 +118,7 @@ async def gpt(update: Update, context: CallbackContext):
             [InlineKeyboardButton(text="Pro Subscription - 119,900 IR Toman - 35 TRX", callback_data="plan_3")],
             [InlineKeyboardButton(text="Back", callback_data="subscription_options")]
         ]
-        reply = subscription_costs[user['lang']]
+        reply = subscription_costs[context.user_data['lang']]
     elif query.data == "gpt_4_m":
         plans_keyboard = [
             [InlineKeyboardButton(text="Economic Subscription - 14,900 IR Toman - 5 TRX", callback_data="plan_4")],
@@ -134,7 +126,7 @@ async def gpt(update: Update, context: CallbackContext):
             [InlineKeyboardButton(text="Pro Subscription - 49,900 IR Toman - 15 TRX", callback_data="plan_6")],
             [InlineKeyboardButton(text="Back", callback_data="subscription_options")]
         ]
-        reply = subscription_costs[user['lang']]
+        reply = subscription_costs[context.user_data['lang']]
     
     if query.data == "subscription_options":
         await show_subscription_options(update, context, query)
